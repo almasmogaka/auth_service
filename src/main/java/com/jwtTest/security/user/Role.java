9562731991fceeec9dd5533b8cbd1338487cc842
ANGWENYI ALMAS <almasdev2018@gmail.com>
2023-05-09 23:43:37 +0300

    spring jwt auth service

---
+++ src/main/java/com/jwtTest/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.jwtTest.security.user;
+
+public enum Role {
+    USER, ADMIN
+
+}
---
+++ src/main/java/com/jwtTest/security/user/User.java
@@ -0,0 +1,86 @@
+package com.jwtTest.security.user;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(name = "USER_ID")
+    private Integer id;
+    @Column(name = "USER_NAMES")
+    private String names;
+    @Column(name = "USER_EMAIL")
+    private String email;
+    @Column(name = "USER_ID_NO")
+    private Integer idNo;
+    @Column(name = "USER_USERNAME")
+    private String username;
+    @Column(name = "USER_PHONE")
+    private String phone;
+    @Column(name = "USER_PASSWORD")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    
+    }
+}
